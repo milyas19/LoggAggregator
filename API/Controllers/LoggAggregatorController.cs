7e3d77fd25e6897d4065022bffe0ba303ea4aac5
Muhammad Ilyas <muhammad.ilyas@capgemini.com>
2020-09-18 22:09:26 +0200

    added missing files

---
+++ API/Controllers/LoggAggregatorController.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Entities;
+using Microsoft.AspNetCore.Mvc;
+using Persistence.Data;
+
+namespace API.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class LoggAggregatorController : ControllerBase
+    {
+        private readonly LoggAggregatorContext _loggAggregatorContext;
+        public LoggAggregatorController(LoggAggregatorContext loggAggregatorContext)
+        {
+            _loggAggregatorContext = loggAggregatorContext;
+        }
+
+        // GET: api/<LoggAggregatorController>
+        [HttpGet]
+        public IEnumerable<string> Get()
+        {
+            return new string[] { "value1", "value2" };
+        }
+
+        // GET api/<LoggAggregatorController>/5
+        [HttpGet("{id}")]
+        public ActionResult<LoggAggregator> Get(int id)
+        {
+            var log = _loggAggregatorContext.LoggAggregators.Where(x => x.Id == id);
+            return Ok(log);
+        }
+
+        // POST api/<LoggAggregatorController>
+        [HttpPost]
+        public void Post([FromBody] string value)
+        {
+        }
+    }
+}
---
+++ API/Startup.cs
@@ -0,0 +1,43 @@
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+using Persistence.Data;
+
+namespace API
+{
+    public class Startup
+    {
+        public Startup(IConfiguration configuration)
+        {
+            Configuration = configuration;
+        }
+
+        public IConfiguration Configuration { get; }
+
+        public void ConfigureServices(IServiceCollection services)
+        {
+            services.AddDbContext<LoggAggregatorContext>
+                (options => options.UseSqlServer(Configuration.GetConnectionString("DefaultConnection")));
+            
+            services.AddHealthChecks();
+            
+            services.AddMvc(options => options.EnableEndpointRouting = false);
+        }
+
+        public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
+        {
+            if (env.IsDevelopment())
+            {
+                app.UseDeveloperExceptionPage();
+            }
+            
+            app.UseHttpsRedirection();
+            app.UseRouting();
+            app.UseMvc();
+
+        }
+    }
+}
